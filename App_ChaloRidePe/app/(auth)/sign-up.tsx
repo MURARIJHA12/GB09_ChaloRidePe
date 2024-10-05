f4bded2eb76815bbb064d29c4dd0b7fb9cd56279
meenakshi bharadwaj <meenakshi.2325mca100@kiet.edu>
2024-10-06 01:25:49 +0530

    Added UI for Auth Screens

--- App_ChaloRidePe/app/(auth)/sign-up.tsx
+++ App_ChaloRidePe/app/(auth)/sign-up.tsx
@@ -1,8 +1,10 @@
+import CustomButton from "@/components/CustomButton";
 import InputField from "@/components/InputField";
+import OAuth from "@/components/OAuth";
 import { icons, images } from "@/constants";
+import { Link } from "expo-router";
 import { useState } from "react";
-import {Image, ScrollView, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {Image, ScrollView, Text, View} from "react-native";
 
 const SignUp = () => {
     const [form, setForm] = useState({
@@ -10,11 +12,16 @@ const SignUp = () => {
         email: "",
         password: ""
     });
+
+    const onSignUpPress = async () => {
+
+    }
+
     return (
         <ScrollView className="flex-1 bg-white">
             <View className="flex-1 bg-white">
                 <View className="relative w-full h-[250px]">
-                    <Image source={images.signUpCar} className="z=0 w-full h-[250px]"/>
+                    <Image source={images.signUpCar} className="z-0 w-full h-[250px]"/>
                     <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
                         Create Your Account
                     </Text>
@@ -34,7 +41,7 @@ const SignUp = () => {
                     <InputField
                         label="Email"
                         placeholder="Enter Your email"
-                        icon={icons.person}
+                        icon={icons.email}
                         value={form.email}
                         onChangeText={(value)=>setForm({
                             ...form, 
@@ -43,14 +50,24 @@ const SignUp = () => {
                     />
                     <InputField
                         label="Password"
-                        placeholder="Enter Your name"
-                        icon={icons.person}
+                        placeholder="Enter Your Password"
+                        icon={icons.lock}
+                        secureTextEntry={true}
                         value={form.password}
                         onChangeText={(value)=>setForm({
                             ...form, 
                             password: value
                         })}
                     />
+
+                    <CustomButton title="Sign Up" onPress={onSignUpPress} className="mt-5"/>
+
+                    <OAuth/>
+
+                    <Link href={"/sign-in"} className="text-lg text-center text-general-200 mt-10">
+                        <Text>Already have an account? </Text>
+                        <Text className="text-primary-500">Log In</Text>
+                    </Link>
                 </View>
             </View>
         </ScrollView>
