a5a1e15a090bdbf036d1609bb627ea94edc1c067
meenakshi bharadwaj <meenakshi.2325mca100@kiet.edu>
2024-10-13 20:17:19 +0530

    Added Map for User Location in Home Page

--- App_ChaloRidePe/app/(root)/(tabs)/home.tsx
+++ App_ChaloRidePe/app/(root)/(tabs)/home.tsx
@@ -1,3 +1,5 @@
+import GoogleTextInput from "@/components/GoogleTextInput";
+import Map from "@/components/Map";
 import RideCard from "@/components/RideCard";
 import { icons, images } from "@/constants";
 import { useAuth, useUser } from "@clerk/clerk-expo";
@@ -129,6 +131,8 @@ export default function Page() {
     router.replace("/(auth)/sign-in");
   };
 
+  const handleDestinationPress = () => {};
+
   return (
     <SafeAreaView>
       <FlatList
@@ -172,6 +176,25 @@ export default function Page() {
                 <Image source={icons.out} className="w-4 h-4" />
               </TouchableOpacity>
             </View>
+
+            <GoogleTextInput
+              icon={icons.search}
+              containerStyle="bg-white shadow-md shadow-neutral-300"
+              handlePress={handleDestinationPress}
+            />
+
+            <>
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Your Current Location
+              </Text>
+              <View className="flex flex-row items-center bg-transparent h-[300px]">
+                <Map />
+              </View>
+
+              <Text className="text-xl font-JakartaBold mt-5 mb-3">
+                Recent Rides
+              </Text>
+            </>
           </>
         )}
       />
